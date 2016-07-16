2ad86a7dfabcd2170b3a7bbba240729effccac2f
Melynda Lindhorst <mlindhorst.24587@gmail.com>
2016-07-16 11:05:46 -0600

    Fixed the example code to work with our current architecture.
    Keep an eye on your globals and local variables. :tangerine:

--- Main.js
+++ Main.js
@@ -1,37 +1,39 @@
 interactive = true;
 var stage;
+var renderer;
 
 function Main() {
 	this.renderer = PIXI.autoDetectRenderer(
 		500,
 		500,
 		{backgroundColor : 0x1099bb}
-	);
-	
+	);	
+	renderer = this.renderer;
 	document.body.appendChild(this.renderer.view);
 	this.stage = new PIXI.Container();
-	stage = this.stage; //assign global
+	stage = this.stage;
 	
-	// Keep this in here?
-	//this.loadSpriteSheet();
+	// Keep this in here? Or move to GameController?
+	this.loadSpriteSheet();
 	
 	// TODO: Setup key events. In different key event class?
 	// TODO: Call gamecontroller to start setting up stuff and load level controller.
 }
 
+Main.prototype.animate = function() {
+	renderer.render(stage);
+	requestAnimationFrame(Main.prototype.animate.bind(this));
+};
+
 Main.prototype.loadSpriteSheet = function() {
 	loader = PIXI.loader;
 	loader.add('puppy', "resources/puppytemp.png");
-	loader.onComplete = this.spriteSheetLoaded.bind(this);
+	loader.once('complete', this.spriteSheetLoaded);
 	loader.load();
 };
 
+// TODO: Fix this. spriteSheetLoaded requires local variables because when it's called, its parent (accessed by the "this" keyword) is the PIXI.loader, which messes everything up.
 Main.prototype.spriteSheetLoaded = function() {
-	this.gamecontroller = new GameController(this.stage);	
-	requestAnimFrame(this.update.bind(this));
-};
-
-Main.prototype.update = function() {
-	this.renderer.render(this.stage);
-	requestAnimFrame(this.update.bind(this));
+	this.gamecontroller = new GameController(stage);	
+	requestAnimationFrame(Main.prototype.animate.bind(this));
 };
--- js/GameController.js
+++ js/GameController.js
@@ -1,17 +1,16 @@
-function GameController(stage) {
-	
-	this.stage = stage;
+function GameController(stage) {	
+	this.stage = stage;	
 	
 	var texture = PIXI.Texture.fromImage("resources/puppytemp.png");
 	var puppy = new PIXI.Sprite(texture);
 	
 	// center the sprite's anchor point
-	bunny.anchor.x = 0.5;
-	bunny.anchor.y = 0.5;
+	puppy.anchor.x = 0.5;
+	puppy.anchor.y = 0.5;
 
 	// move the sprite to the center of the screen
-	bunny.position.x = 200;
-	bunny.position.y = 150;
+	puppy.position.x = 200;
+	puppy.position.y = 150;
 
-	stage.add(puppy);
+	this.stage.addChild(puppy);
 }
