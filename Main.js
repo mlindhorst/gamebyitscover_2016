e6183e6ef853698817237eddd2805cb21ec7b706
Melynda Lindhorst <mlindhorst.24587@gmail.com>
2016-07-22 20:07:45 -0600

    Main!

--- Main.js
+++ Main.js
@@ -65,6 +65,43 @@ Main.prototype.loadSpriteSheet = function() {
 	loader.add('facilityfanblades', "resources/Levels/Facility/Fan_Blades.png");
 	loader.add('facilityfancover', "resources/Levels/Facility/Fan_Cover.png");
 	
+	// Level 2 - Mountain
+	loader.add('mountainbg', "resources/Levels/Mountains/MountainsBG_01.png");
+	loader.add('mountainfg', "resources/Levels/Mountains/MountainsBG_02.png");
+	loader.add('fallingrock1', "resources/Levels/Mountains/fallingrock_01.png");
+	loader.add('fallingrock2', "resources/Levels/Mountains/fallingrock_02.png");
+	loader.add('fallingrock3', "resources/Levels/Mountains/fallingrock_03.png");	
+	loader.add('fallingrock4', "resources/Levels/Mountains/fallingrock_04.png");
+	loader.add('largeboulder', "resources/Levels/Mountains/largeboulder.png");
+	loader.add('smoke1', "resources/Levels/Mountains/smokepoof_01.png");
+	loader.add('smoke2', "resources/Levels/Mountains/smokepoof_02.png");
+	loader.add('cloud1', "resources/Levels/Mountains/Cloud_01.png");
+	loader.add('cloud2', "resources/Levels/Mountains/Cloud_02.png");
+	loader.add('cloud3', "resources/Levels/Mountains/Cloud_03.png");	
+	loader.add('beartrapl', "resources/Levels/Mountains/beartrap_left.png");
+	loader.add('beartrapr', "resources/Levels/Mountains/beartrap_right.png");
+	loader.add('beartrapm', "resources/Levels/Mountains/beartrap_middle.png");	
+	loader.add('bh_boulder1', "resources/Levels/Mountains/Boulderholder_boulder_01.png");
+	loader.add('bh_rope1', "resources/Levels/Mountains/Boulderholder_rope_01.png");
+	loader.add('bhtree_rope1', "resources/Levels/Mountains/Boulderholder_treerope_01.png");
+	loader.add('bh_boulder2', "resources/Levels/Mountains/Boulderholder_boulder_02.png");
+	loader.add('bh_rope2', "resources/Levels/Mountains/Boulderholder_rope_02.png");
+	loader.add('bhtree_rope2', "resources/Levels/Mountains/Boulderholder_treerope_02.png");
+	
+	// Level 3 - Sky
+	
+	// Level 4 - Water
+	loader.add('waterbg', "resources/Levels/Water/WaterBG_Water.png");
+	loader.add('waterfg', "resources/Levels/Water/WaterBG_Foreground.png");
+	loader.add('barrel', "resources/Levels/Water/Barrel.png");
+	loader.add('bubble_1', "resources/Levels/Water/Bubble_01.png");
+	loader.add('bubble_2', "resources/Levels/Water/Bubble_02.png");
+	loader.add('fish_1', "resources/Levels/Water/Fish_01.png");
+	loader.add('fish_2', "resources/Levels/Water/Fish_02.png");
+	loader.add('fish_3', "resources/Levels/Water/Fish_03.png");
+	loader.add('ooze_1', "resources/Levels/Water/Ooze_01.png");
+	loader.add('ooze_2', "resources/Levels/Water/Ooze_02.png");
+	
 	loader.once('complete', this.spriteSheetLoaded);
 	loader.load();
 };
